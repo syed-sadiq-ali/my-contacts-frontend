922e14ccb26be0bc71ee31eba35350462a4293bb
syed-sadiq-ali <sadiq.ali@tenpearls.com>
2018-11-12 15:42:17 +0500

    Add Login component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from '../Login';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about/">About</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Route path="/" exact component={Index} />
-          <Route path="/about/" component={About} />
-        </div>
+        <Route path="/" exact component={Login} />
       </Router>
     );
   }
---
+++ src/components/Login/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return(
+      <div>
+        <p>Welcome to My Contacts!</p>
+        <p>Log in to view your contacts</p>
+        <button>Log in</button>
+      </div>
+    );
+  }
+}
+
+export default Login;
