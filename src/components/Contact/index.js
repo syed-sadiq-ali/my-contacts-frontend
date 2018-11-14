160133b89b96832c3fefe681a709c9f19c6e716c
syed-sadiq-ali <sadiq.ali@tenpearls.com>
2018-11-14 14:48:35 +0500

    Add modal for deleting contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import EditContactModal from '../EditContactModal';
+import DeleteContactModal from '../DeleteContactModal';
 
 class Contact extends Component {
   constructor() {
     super();
     this.state = {
-      isOpen: false,
+      isEditOpen: false,
+      isDeleteOpen: false,
     };
   }
 
-  toggleModal = () => {
+  toggleEditModal = () => {
     this.setState({
-      isOpen: !this.state.isOpen,
+      isEditOpen: !this.state.isEditOpen,
+    });
+  };
+
+  toggleDeleteModal = () => {
+    this.setState({
+      isDeleteOpen: !this.state.isDeleteOpen,
     });
   };
 
@@ -21,15 +29,18 @@ class Contact extends Component {
       <div>
         <h4>{`${firstName} ${lastName}`}</h4>
         <p>{`${mobileNumber}, ${email}, ${work}`}</p>
-        <button onClick={this.toggleModal}>Edit</button> <span></span>
-        <button>Delete</button>
+        <button onClick={this.toggleEditModal}>Edit</button> <span></span>
+        <button onClick={this.toggleDeleteModal}>Delete</button>
 
         <EditContactModal
-          open={this.state.isOpen}
-          onClose={this.toggleModal}
+          open={this.state.isEditOpen}
+          onClose={this.toggleEditModal}
           contact={this.props.contact}
         />
-
+        <DeleteContactModal
+          open={this.state.isDeleteOpen}
+          onClose={this.toggleDeleteModal}
+        />
       </div>
 );
   }
---
+++ src/components/DeleteContactModal/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+class DeleteContactModal extends React.Component {
+  
+  render() {
+    const { open, onClose } = this.props;
+
+    return (
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Delete Contact</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+              Are you sure you want to delete?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>
+            Cancel
+          </Button>
+          <Button onClick={onClose} color="secondary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default DeleteContactModal;
